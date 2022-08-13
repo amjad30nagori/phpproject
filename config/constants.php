8e132b4443370b0f22c852857ea4aa658985d10d
amjad30nagori <43353561+amjad30nagori@users.noreply.github.com>
2022-08-13 11:09:49 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 define('HOST', 'testamjsql.mysql.database.azure.com');
 define('USER', 'amjad@testamjsql');
-define('PASSWORD', 'Qasim@269223459');
+define('PASSWORD', 'Password@123');
 define('DATABASE_NAME', 'ecommerceapp');
 
 define('CURRENCY', 'Rs');
